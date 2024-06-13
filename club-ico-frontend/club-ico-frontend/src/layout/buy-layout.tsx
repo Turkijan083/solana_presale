c97a34facccf31b604b68bd356dcd3d51b5c4502
Turkijan_DEV <162873906+Turkijan083@users.noreply.github.com>
2024-06-13 10:48:24 -0700

    css_update
    css_update

--- club-ico-frontend/club-ico-frontend/src/layout/buy-layout.tsx
+++ club-ico-frontend/club-ico-frontend/src/layout/buy-layout.tsx
@@ -61,8 +61,12 @@ export default function BuyLayout() {
 
   return (
     <div className="w-full h-[800px] sm:h-120 max-w-[700px] rounded-3xl bg-[#dae3eaa0] px-8 sm:px-12 py-8 flex flex-col gap-3 sm:gap-6">
-      <span className="font-inter font-bold text-[#000000] text-sm sm:text-lg">
-        Miron Presale Stage ( Stage 1 )
+      <span className="font-inter font-bold text-[#000000] text-sm sm:text-lg text-center">
+        Miron Presale Stage 
+      </span>
+
+      <span className="font-inter font-bold text-[#000000] text-sm sm:text-lg text-center">
+        Stage 1
       </span>
 
       <StatsLayout />
